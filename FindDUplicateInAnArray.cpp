7151a39859da2b356c0e09750b7442526268660b
vvsailesh <96788213+vvsailesh@users.noreply.github.com>
2021-12-28 22:20:31 +0530

    Create FindDUplicateInAnArray.cpp

---
+++ FindDUplicateInAnArray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#Include <stdio.h>
+#include<string.h>
+
+using namespace std;
+
+int main{
+  cout<<"This program prints the duplicate in an array of size n with n-1 unique elements and a single duplicate" <<endl;
+  
+  
+ return 0; 
+}
